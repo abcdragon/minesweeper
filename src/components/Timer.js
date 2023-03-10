d714e27b763ccc44b6cf8276e480edad285bb73a
timsort <timsort@kookmin.ac.kr>
2023-03-10 14:04:38 +0900

    타이머 구현

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,3 +1,32 @@
+import { useState, useEffect, useContext, useRef } from "react";
+
+import { GAME_STATE } from "../lib/variables";
+import { MineInfoContext } from "../lib/MineInfoContext";
+
 export const Timer = () => {
-    return <h1>시간입니다~</h1>
+    const [ time, setTime ] = useState(0);
+    const { mineInfo } = useContext(MineInfoContext);
+    const { game_state } = mineInfo;
+
+    const tick = useRef(null);
+
+    const startTick = () => {
+        tick.current = setInterval(() => {
+            setTime(time => time + 1);
+        }, 1000);
+    }
+
+    const endTick = () => {
+        clearInterval(tick.current);
+    }
+
+    useEffect(() => {
+        if(game_state === GAME_STATE.PROCEED) startTick();
+        else endTick();
+
+
+        if(game_state === GAME_STATE.BEFORE_START) setTime(0);
+    }, [game_state]);
+
+    return <h1>시간 : {time}</h1>
 };
